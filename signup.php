b0a0c4eace4b0475e25b45a6d6a6ecbf8c85c8ea
Vickei63 <vicdavies811@gmail.com>
2026-02-08 14:59:07 +0300

    signup functionality

--- signup.php
+++ signup.php
@@ -1,6 +1,9 @@
 
 
 <?php
+
+    $success = 0;
+    $user = 0;
     
     //SIGNUP
         //1. INSERT DATA
@@ -9,8 +12,44 @@
     if($_SERVER['REQUEST_METHOD'] == 'POST'){
         include "connect.php";
 
-        
+        $username = $_POST['username'];
+        $password = $_POST['password'];
 
+
+        // $sql = "insert into users (username,password)
+        // values('$usern','$passw')";
+        // $result = mysqli_query($conn,$sql);
+
+        // if($result){
+        //     echo "Data inserted successfully !";
+        // }
+        // else{
+        //     echo "data not inserted";
+        // }
+        
+        $sql = "select * from users where username = '$username'";
+        $results = mysqli_query($conn,$sql);
+
+        if($results){
+            $num = mysqli_num_rows($results);
+             if($num > 0){
+                echo "User already exists";
+                $user = 1;
+             }
+        }
+        else{
+            
+            $sql = "INSERT INTO `users` (username, password) 
+                VALUES (NULL, '$username', '$password') ";
+            $results = mysqli_query($conn,$sql);
+
+            if($results){
+                echo "Data inserted successfully !";
+            }
+            else{
+                echo "data not inserted";
+            }
+        }
     }
 
 
@@ -31,12 +70,14 @@
 
         <div class="form-group">
             <label for="">Username:</label>
-            <input type="text" placeholder="Enter your Username" class="form-control">
+            <input type="text" name="username"
+            placeholder="Enter your Username" class="form-control">
         </div>
 
         <div class="form-group">
             <label for="">Password:</label>
-            <input type="text" placeholder="Enter your Password" class="form-control">
+            <input type="text" name="password"
+            placeholder="Enter your Password" class="form-control">
         </div>
 
             <button class="btn btn-primary mt-5 w-100">Sign Up</button>
